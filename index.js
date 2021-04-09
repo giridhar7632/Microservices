13929ccf5714b395137f237b3d765a84dd7fa461
Talla Giridhar <giridhar.talla2002@gmail.com>
2021-04-09 13:01:25 +0000

    added bulma css

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express")
+const path = require("path")
 const port = process.env.PORT || 3000
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.listen(port, (err) => {
   if(err) throw err
